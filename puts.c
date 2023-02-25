e9c171be921afaa2ae163211a673e653b0591680
richard-coder-oss <richardonsare15@gmail.com>
2023-02-25 06:15:28 -0800

    man puts

---
+++ puts.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+/*writes the string s and a trailing newline to stdout */
+/* puts function example explained */
+
+int main() {
+    char str[] = "Hello, World!";
+    puts(str);
+    return 0;
+}
