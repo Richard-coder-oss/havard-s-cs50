0589064f75696a0540d933a7ecb043cfaf510705
richard-coder-oss <richardonsare15@gmail.com>
2023-02-25 06:29:14 -0800

    man putchar

---
+++ putchar.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/*writes a single character to the standard output stream*/
+/*putchar example below*/
+
+int main() {
+    char c = 'H';
+    putchar(c);
+    putchar('\n'); /* write a newline character */
+    return 0;
+}
+
+
